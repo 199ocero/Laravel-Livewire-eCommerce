aadd2e1c8697261ad4c0d594ac595f363c13edbb
freelanceocero <199ocero@gmail.com>
2023-01-03 16:31:08 +0800

    ⚡️ add media library

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     // Function for formatted price to show as PHP currency
     public function formattedPrice()
